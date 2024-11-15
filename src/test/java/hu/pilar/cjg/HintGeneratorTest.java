ba6426f3cf7402bdeb1011ae5a485d914fe7b884
Janos Cserep <cserepj@pilar.hu>
2024-11-15 23:02:24 +0100

    upgraded dependencies, removed hamcrest

--- src/test/java/hu/pilar/cjg/HintGeneratorTest.java
+++ src/test/java/hu/pilar/cjg/HintGeneratorTest.java
@@ -16,160 +16,158 @@
  */
 package hu.pilar.cjg;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.xml.bind.annotation.*;
-import org.junit.jupiter.api.Test;
-
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class HintGeneratorTest {
 
-    @Test
-    void testNonJaxb() {
-        HintGenerator hg = new HintGenerator(new ObjectMapper());
-        var hint = hg.getHintsFor(Object.class);
-        assertFalse(hint.isPresent());
-    }
+  @Test
+  void testNonJaxb() {
+    HintGenerator hg = new HintGenerator(new ObjectMapper());
+    var hint = hg.getHintsFor(Object.class);
+    assertFalse(hint.isPresent());
+  }
 
-    @Test
-    void testConstructorWithSubClassFinder() {
-        HintGenerator hg = new HintGenerator(new ObjectMapper(), new ISubclassFinder() {
-            @Override
-            public <T> Set<Class<? extends T>> findClassesThatExtend(final Class<T> parent) {
+  @Test
+  void testConstructorWithSubClassFinder() {
+    HintGenerator hg =
+        new HintGenerator(
+            new ObjectMapper(),
+            new ISubclassFinder() {
+              @Override
+              public <T> Set<Class<? extends T>> findClassesThatExtend(final Class<T> parent) {
                 return Set.of();
-            }
-        });
-        var hint = hg.getHintsFor(DefaultNameTestClass.class);
-        assertThat(hint.isPresent(), is(true));
-        assertThat(hint.get().toJson(), equalTo(
-            "var tags = {\"!top\":[\"defaultNameTestClass\"],\"!attrs\":{},\"defaultNameTestClass\":{\"attrs\":{},\"children\":[\"nestedParameterized\"]},\"nestedParameterized\":{\"attrs\":{},\"children\":[]}};"));
-    }
+              }
+            });
+    var hint = hg.getHintsFor(DefaultNameTestClass.class);
+    assertThat(hint).isPresent();
+    assertThat(hint.orElseThrow().toJson())
+        .isEqualTo(
+            "var tags = {\"!top\":[\"defaultNameTestClass\"],\"!attrs\":{},\"defaultNameTestClass\":{\"attrs\":{},\"children\":[\"nestedParameterized\"]},\"nestedParameterized\":{\"attrs\":{},\"children\":[]}};");
+  }
 
+  @Test
+  void test() {
+    HintGenerator hg = new HintGenerator(new ObjectMapper());
 
-    @Test
-    void test() {
-        HintGenerator hg = new HintGenerator(new ObjectMapper());
+    XmlHint hint = hg.getHintsFor(TestClass.class).orElse(null);
+    assertNotNull(hint);
+    assertThat(hint.toJson())
+        .isEqualTo(
+            "var tags = {"
+                + "\"!top\":[\"C\"],"
+                + "\"!attrs\":{},"
+                + "\"A\":{\"attrs\":{\"nextValue\":[\"ONE\",\"TWO\",\"THREE\"],\"value\":[\"ONE\",\"TWO\",\"THREE\"]},\"children\":[]},"
+                + "\"C\":{\"attrs\":{\"boole\":[\"true\",\"false\"],\"boole2\":[\"true\",\"false\"],\"value3\":[]},\"children\":[\"A\",\"D\",\"b\"]},"
+                + "\"D\":{\"attrs\":{\"nextValue\":[\"ONE\",\"TWO\",\"THREE\"],\"value\":[\"ONE\",\"TWO\",\"THREE\"]},\"children\":[]},"
+                + "\"b\":{\"attrs\":{\"nextValue\":[\"ONE\",\"TWO\",\"THREE\"],\"value\":[\"ONE\",\"TWO\",\"THREE\"]},\"children\":[]}"
+                + "};");
 
-        XmlHint hint = hg.getHintsFor(TestClass.class).orElse(null);
-        assertNotNull(hint);
-        assertThat(hint.toJson(), equalTo("var tags = {"
-            + "\"!top\":[\"C\"],"
-            + "\"!attrs\":{},"
-            + "\"A\":{\"attrs\":{\"nextValue\":[\"ONE\",\"TWO\",\"THREE\"],\"value\":[\"ONE\",\"TWO\",\"THREE\"]},\"children\":[]},"
-            + "\"C\":{\"attrs\":{\"boole\":[\"true\",\"false\"],\"boole2\":[\"true\",\"false\"],\"value3\":[]},\"children\":[\"A\",\"D\",\"b\"]},"
-            + "\"D\":{\"attrs\":{\"nextValue\":[\"ONE\",\"TWO\",\"THREE\"],\"value\":[\"ONE\",\"TWO\",\"THREE\"]},\"children\":[]},"
-            + "\"b\":{\"attrs\":{\"nextValue\":[\"ONE\",\"TWO\",\"THREE\"],\"value\":[\"ONE\",\"TWO\",\"THREE\"]},\"children\":[]}"
-            + "};"));
-
-        hg = new HintGenerator(new ObjectMapper(), (attributeName, type) -> {
-            if ("value3".equals(attributeName)) {
+    hg =
+        new HintGenerator(
+            new ObjectMapper(),
+            (attributeName, type) -> {
+              if ("value3".equals(attributeName)) {
                 Set<String> s = new LinkedHashSet<>();
                 s.add("1979");
                 s.add("1980");
                 s.add("1981");
                 return s;
-            }
-            return null;
-        });
-        hint = hg.getHintsFor(TestClass.class).orElse(null);
-        assertNotNull(hint);
-        assertEquals("var tags = {"
-                + "\"!top\":[\"C\"],"
-                + "\"!attrs\":{},"
-                + "\"A\":{\"attrs\":{\"nextValue\":[\"ONE\",\"TWO\",\"THREE\"],\"value\":[\"ONE\",\"TWO\",\"THREE\"]},\"children\":[]},"
-                + "\"C\":{\"attrs\":{\"boole\":[\"true\",\"false\"],\"boole2\":[\"true\",\"false\"],\"value3\":[\"1979\",\"1980\",\"1981\"]},\"children\":[\"A\",\"D\",\"b\"]},"
-                + "\"D\":{\"attrs\":{\"nextValue\":[\"ONE\",\"TWO\",\"THREE\"],\"value\":[\"ONE\",\"TWO\",\"THREE\"]},\"children\":[]},"
-                + "\"b\":{\"attrs\":{\"nextValue\":[\"ONE\",\"TWO\",\"THREE\"],\"value\":[\"ONE\",\"TWO\",\"THREE\"]},\"children\":[]}"
-                + "};",
-            hint.toJson());
+              }
+              return null;
+            });
+    hint = hg.getHintsFor(TestClass.class).orElse(null);
+    assertNotNull(hint);
+    assertEquals(
+        "var tags = {"
+            + "\"!top\":[\"C\"],"
+            + "\"!attrs\":{},"
+            + "\"A\":{\"attrs\":{\"nextValue\":[\"ONE\",\"TWO\",\"THREE\"],\"value\":[\"ONE\",\"TWO\",\"THREE\"]},\"children\":[]},"
+            + "\"C\":{\"attrs\":{\"boole\":[\"true\",\"false\"],\"boole2\":[\"true\",\"false\"],\"value3\":[\"1979\",\"1980\",\"1981\"]},\"children\":[\"A\",\"D\",\"b\"]},"
+            + "\"D\":{\"attrs\":{\"nextValue\":[\"ONE\",\"TWO\",\"THREE\"],\"value\":[\"ONE\",\"TWO\",\"THREE\"]},\"children\":[]},"
+            + "\"b\":{\"attrs\":{\"nextValue\":[\"ONE\",\"TWO\",\"THREE\"],\"value\":[\"ONE\",\"TWO\",\"THREE\"]},\"children\":[]}"
+            + "};",
+        hint.toJson());
+  }
 
+  public enum TestEnum {
+    ONE,
+    TWO,
+    THREE
+  }
+
+  @XmlTransient
+  abstract static class TestAbstract {
+
+    TestEnum value, key;
+
+    @XmlAttribute
+    public TestEnum getValue() {
+      return value;
     }
 
-    public enum TestEnum {
-        ONE, TWO, THREE
+    @XmlAttribute(name = "nextValue")
+    public TestEnum getKey() {
+      return key;
+    }
+  }
+
+  @XmlRootElement(name = "A")
+  static class TestA extends TestAbstract {}
+
+  @XmlRootElement(name = "b")
+  static class B extends TestAbstract {}
+
+  @XmlRootElement(name = "C")
+  static class TestClass {
+
+    @XmlElementRef
+    public List<TestAbstract> getList() {
+      return null;
     }
 
-    @XmlTransient
-    abstract static class TestAbstract {
-
-        TestEnum value, key;
-
-        @XmlAttribute
-        public TestEnum getValue() {
-            return value;
-        }
-
-        @XmlAttribute(name = "nextValue")
-        public TestEnum getKey() {
-            return key;
-        }
-
+    @XmlAttribute
+    public Boolean getBoole() {
+      return true;
     }
 
-    @XmlRootElement(name = "A")
-    static class TestA extends TestAbstract {
+    @XmlAttribute
+    public boolean isBoole2() {
+      return true;
     }
 
-    @XmlRootElement(name = "b")
-    static class B extends TestAbstract {
+    @XmlAttribute
+    public String getValue3() {
+      return "";
     }
 
-    @XmlRootElement(name = "C")
-    static class TestClass {
+    @XmlElement(name = "D")
+    public TestA getEnum() {
+      return null;
+    }
+  }
 
-        @XmlElementRef
-        public List<TestAbstract> getList() {
-            return null;
-        }
+  @XmlRootElement()
+  static class NestedParameterized<T> {}
 
-        @XmlAttribute
-        public Boolean getBoole() {
-            return true;
-        }
+  @XmlRootElement
+  static class DefaultNameTestClass {
 
-        @XmlAttribute
-        public boolean isBoole2() {
-            return true;
-        }
-
-        @XmlAttribute
-        public String getValue3() {
-            return "";
-        }
-
-        @XmlElement(name = "D")
-        public TestA getEnum() {
-            return null;
-        }
-
+    @XmlElementRef()
+    public List<NestedParameterized<TestA>> getParameterizedList() {
+      return List.of();
     }
 
-    @XmlRootElement()
-    static class NestedParameterized<T> {
-
+    @XmlElementRef()
+    public List getParameterizedListToTriggerWarning() {
+      return List.of();
     }
-
-    @XmlRootElement
-    static class DefaultNameTestClass {
-
-        @XmlElementRef()
-        public List<NestedParameterized<TestA>> getParameterizedList() {
-            return List.of();
-        }
-
-        @XmlElementRef()
-        public List getParameterizedListToTriggerWarning() {
-            return List.of();
-        }
-
-
-    }
-
+  }
 }
